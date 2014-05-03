fa3084573c180617e37e6773da3cd1e824557c62
Daniel Erickson <techwraithpdx@gmail.com>
2014-05-02 23:57:19 -0700

    stuff

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ module.exports = function (dir) {
   atomify({
     server: {
       lr: {
-        patterns: [dir + '/**']
-      , port: 4001
+        port: 4001
       }
-      , port: 4000
+    , port: 4000
     }
   , css: {
       entry: path.join(dir, 'example/entry.css')
+    , alias: '/bundle.css'
     , debug: true
     }
   , js: {
       entry: path.join(dir, 'example/entry.js')
+    , alias: '/bundle.js'
     , debug: true
     }
   })
