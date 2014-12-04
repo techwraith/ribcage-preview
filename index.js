bc14ac51df7eb15120b49140d7aea0598d7e16d8
Joey Baker <joey@byjoeybaker.com>
2014-12-04 13:47:39 -0800

    Updated atomify has a different browserSync opts

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
+'use strict';
+
 var atomify = require('atomify')
   , path = require('path')
-  , os = require('os')
 
 module.exports = function (dir) {
-  var hostname = os.hostname()
-
-  // hack--because sometimes hostnames end in .local and sometimes they don't...
-  if (hostname.slice(-1 * '.local'.length) !== '.local')
-    hostname = hostname + '.local'
-
   atomify({
     server: {
       lr: {
@@ -16,7 +11,6 @@ module.exports = function (dir) {
       , port: 4001
       }
     , port: 4000
-    , url: 'http://' + os.hostname()
     }
   , css: {
       entry: path.join(dir, 'example/entry.css')
