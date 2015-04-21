a463ac9289bd7170713c496a29fcb2fb6c51ec1e
Joey Baker <joey@byjoeybaker.com>
2015-04-21 12:45:19 -0700

    fix: server HTML now properly resolves requires
    …through a massive hack. OMG this is dirty

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var atomify = require('atomify')
   , fs = require('fs')
   , React = require('react')
   , babel = require('babel-core')
+  , cwd = process.cwd()
   , internals = {}
 
 internals.findFirstFile = function findFirstFile (dir, filenames){
@@ -80,8 +81,18 @@ module.exports = function ribcagePreview (options) {
 
         // we'll get back a string of JS that needs to be run here, so … eval it
         /* eslint-disable no-eval */
-        reactComponent = eval(result.code)
+        reactComponent = eval(result.code
         /* eslint-enable no-eval */
+          // FIXME: OMG this is hacky. like… really bad. There might be a better
+          // way to do this by deleting the jsComponent from the require.cache
+          // and requring the file each time with the babel require hook in
+          // place… but I'm not sure if that's better or not.
+
+          // get node modules relative to the current working directory
+          .replace(/require\((['"])([A-z])/g, 'require($1' + path.join(cwd, '/node_modules') + '/$2')
+          // get modules relative to the component
+          .replace(/require\((['"])([.])/g, 'require($1' + path.dirname(jsComponent) + '/$2')
+        )
 
         if (paths.css) html += '<link rel="stylesheet" href="' + paths.css + '" />'
         html += '<div id="app">' + React.renderToString(React.createElement(reactComponent)) + '</div>'
