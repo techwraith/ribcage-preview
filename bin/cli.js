27362f7375babb07d2b40f03075110d730af9353
Joey Baker <joey@byjoeybaker.com>
2015-05-18 15:24:37 -0700

    add: warn on CLI startup if dir doesn’t look valid

--- bin/cli.js
+++ bin/cli.js
@@ -7,9 +7,15 @@ var server = require('../index.js')
     boolean: ['s', 'client-jsx', 'r', 'react-router', 'no-debug']
   })
   , path = require('path')
+  , fs = require('fs')
+  , cwd = process.cwd()
+  , dir = path.join(cwd, args._[0])
+
+if (!fs.statSync(dir).isDirectory()) throw new Error('Must pass a directory')
+if (!fs.existsSync(path.join(cwd, 'node_modules'))) throw new Error('Must be run from a directory with `node_modules`')
 
 server({
-  dir: path.join(process.cwd(), args._[0])
+  dir: dir
   , debug: !args['no-debug']
   , enableClientJSX: args.s || args['client-jsx']
   , enableReactRouter: args.r || args['react-router']
